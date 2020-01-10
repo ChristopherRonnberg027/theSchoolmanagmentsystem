e03f612a725c78931d2c0b84da508bd5f1f718da
I.S.op187 <I.S.op187@192.168.0.2>
2020-01-10 18:06:04 +0100

    Owerall db control with JPA implementation

--- src/databaseControl/dbDAO.java
+++ src/databaseControl/dbDAO.java
@@ -6,6 +6,7 @@ import theschoolmanagmentsystem.domain.Course;
 import theschoolmanagmentsystem.domain.Education;
 import theschoolmanagmentsystem.domain.Student;
 import theschoolmanagmentsystem.domain.Teacher;
+import theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException;
 
 /**
  * Acces inteface for database
@@ -26,22 +27,25 @@ public interface dbDAO {
      * teacher. Here is used merge() For creating new Course use createCourse()
      *
      * @param c - Course to edit
+     * @throws java.lang.Exception
      */
-    void editCourse(Course c);
+    void editCourse (Course c) throws Exception ;
     
     /**
      * Delete a course with a speciffic id.
      *
      * @param id
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException
      */
-    void destroyCourse(Long id);
+    void destroyCourse(Long id) throws NonexistentEntityException;
     
     /**
      * Delete a speciffic course
      *
      * @param c - Course to be deleted.
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException
      */
-    void destroyCourse(Course c);
+    void destroyCourse(Course c) throws NonexistentEntityException;
     
     /**
      * Returns Course with the speciffic id.
@@ -88,22 +92,25 @@ public interface dbDAO {
      * createEducation()
      *
      * @param e - Education to edit
+     * @throws java.lang.Exception
      */
-    void editEducation(Education e);
+    void editEducation(Education e) throws Exception;
     
     /**
      * Delete a Education with a speciffic id.
      *
      * @param id
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException
      */
-    void destroyEducation(Long id);
+    void destroyEducation(Long id) throws NonexistentEntityException;
     
     /**
      * Delete a speciffic Education
      *
      * @param e - Education to be deleted.
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException
      */
-    void destroyEducation(Education e);
+    void destroyEducation(Education e) throws NonexistentEntityException;
     
     /**
      * Returns Education with the speciffic id.
@@ -111,14 +118,14 @@ public interface dbDAO {
      * @param id
      * @return Education
      */
-    Course findEducationById(Long id);
+    Education findEducationById(Long id);
     
     /**
      * Get all the Educations
      *
      * @return a List of all Education
      */
-    List<Course> getAllEducations();
+    List<Education> getAllEducations();
     
     /**
      * Returns a List od Educations based on a String parameter.
@@ -126,7 +133,7 @@ public interface dbDAO {
      * @param name
      * @return List of Education
      */
-    List<Course> findEducationByName(String name);
+    List<Education> findEducationByName(String name);
     
     /**
      * @return Education entities count 
@@ -150,33 +157,36 @@ public interface dbDAO {
      * For creating new Student use createStudent()
      * 
      * @param s - Student to edit
+     * @throws java.lang.Exception
      */
-    void editStudent(Student s);
+    void editStudent(Student s) throws Exception;
     
     /**
      * Delete a Student with a speciffic id.
      * @param pn - here the Personal Number is used as id 
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException 
      */
-    void destroyStudent(Long pn);
+    void destroyStudent(Long pn) throws NonexistentEntityException;
     
     /**
      * Delete a speciffic Student
      * 
      * @param s - Student to be deleted.
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException
      */
-    void destroyStudent(Student s);
+    void destroyStudent(Student s) throws NonexistentEntityException;
     
     /**
      * Returns Student with the speciffic id.
      * @param pn - here the Personal Number is used as id 
      * @return Student
      */
-    Course findStudentById(Long pn);
+    Student findStudentById(Long pn);
     
     /**
      * @return a List of all Students
      */
-    List<Course> getAllStudents();
+    List<Student> getAllStudents();
     
     /**
      * Returns a List od Student based on a
@@ -184,7 +194,7 @@ public interface dbDAO {
      * @param firstName
      * @return List of Student
      */
-    List<Course> findStudentByFirstName(String firstName);
+    List<Student> findStudentByFirstName(String firstName);
     
     /**
      * Returns a List od Student based on a
@@ -192,7 +202,7 @@ public interface dbDAO {
      * @param surName
      * @return List of Student
      */
-    List<Course> findStudentBySurName(String surName);
+    List<Student> findStudentBySurName(String surName);
     
     /**
      * @return Student entities count
@@ -213,22 +223,25 @@ public interface dbDAO {
      * is used merge() For creating new Teacher use createTeacher()
      *
      * @param t - Teacher to edit
+     * @throws java.lang.Exception
      */
-    void editTeacher(Teacher t);
+    void editTeacher(Teacher t) throws Exception;
     
     /**
      * Delete a Teacher with a speciffic id.
      *
      * @param pn - here the Personal Number is used as id
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException
      */
-    void destroyTeacher(Long pn);
+    void destroyTeacher(Long pn) throws NonexistentEntityException;
     
     /**
      * Delete a speciffic Teacher
      *
      * @param t - Teacher to be deleted.
+     * @throws theschoolmanagmentsystem.domain.exceptions.NonexistentEntityException
      */
-    void destroyTeacher(Teacher t);
+    void destroyTeacher(Teacher t) throws NonexistentEntityException;
     
     /**
      * Returns Teacher with the speciffic id.
@@ -236,12 +249,12 @@ public interface dbDAO {
      * @param pn - here the Personal Number is used as id
      * @return Teacher
      */
-    Course findTeacherById(Long pn);
+    Teacher findTeacherById(Long pn);
     
     /**
      * @return a List of all the teachers
      */
-    List<Course> getAllTeachers();
+    List<Teacher> getAllTeachers();
     
     /**
      * Returns a List od Teacher based on a String parameter.
@@ -249,7 +262,7 @@ public interface dbDAO {
      * @param firstName
      * @return List of Teacher
      */
-    List<Course> findTeacherByFirstName(String firstName);
+    List<Teacher> findTeacherByFirstName(String firstName);
     
     /**
      * Returns a List od Teacher based on a String parameter.
@@ -257,7 +270,7 @@ public interface dbDAO {
      * @param surName
      * @return List of Teacher
      */
-    List<Course> findTeacherBySurName(String surName);
+    List<Teacher> findTeacherBySurName(String surName);
     
     /**
      * @return Teacher entities count
