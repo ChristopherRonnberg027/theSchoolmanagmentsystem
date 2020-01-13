f7c18e592beda7190c94e1c0d3331eb9b7131107
Fabian' <Fabian'@MSI>
2020-01-13 13:14:19 +0100

    la till grundläggande userEnvironmentInterface och ueTexts klass

---
+++ src/theschoolmanagmentsystem/domain/exceptions/userEnvironment/userEnvironmentInterface.java
@@ -0,0 +1,18 @@
+package theschoolmanagmentsystem.domain.exceptions.userEnvironment;
+
+import java.util.Scanner;
+
+public class userEnvironmentInterface {
+    Scanner sc = new Scanner(System.in);
+    
+    public void displayText(String text){
+        System.out.println(text);
+    }
+    
+    public String getUserInput(){
+        return sc.nextLine();
+    }
+    
+    public userEnvironmentInterface(){
+    }
+}
